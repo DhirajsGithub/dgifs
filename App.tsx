8c526ab582004770b91681693e4dbada30545c08
DhirajsGithub <borsedhiraj123@gmail.com>
2024-11-20 03:10:47 +0530

    trending page beta 1 created

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import {StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-
+import {QueryClientProvider} from '@tanstack/react-query';
+import {QueryClient} from '@tanstack/react-query';
+import Router from './src/routes';
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <View style={styles.bg}>
-      <Text style={{color: "red"}}>App</Text>
-    </View>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
+  );
+};
 
 export default App;
-
-const styles = StyleSheet.create({
-  bg:{
-    backgroundColor: "white",
-    flex: 1,
-  }
-});
